1154572165e9b4923b3b76f081562fd51fa5a85f
Dimitar Karev <d_karev@college.harvard.edu>
2017-10-20 03:08:59 -0400

    check

--- pset3/io61.c
+++ pset3/io61.c
@@ -3,6 +3,8 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <errno.h>
+#include <sys/mman.h>
+
 
 // io61.c
 //    YOUR CODE HERE!
@@ -11,9 +13,26 @@
 // io61_file
 //    Data structure for io61 file wrappers. Add your own stuff.
 
+const size_t CACHESIZE=(1<<12);
+
 struct io61_file {
     int fd;
+    char* cache;
+    unsigned long long first, last;
+    size_t cache_size;
+    off_t pntr;
+    off_t size;
+    int flag;
+    int was_seek;
+
 };
+
+
+size_t min(size_t a, size_t b)
+{
+    if(a<b)return a;
+    return b;
+}
 
 
 // io61_fdopen(fd, mode)
@@ -21,10 +40,25 @@ struct io61_file {
 //    either O_RDONLY for a read-only file or O_WRONLY for a
 //    write-only file. You need not support read/write files.
 
+
+char *file_data;
 io61_file* io61_fdopen(int fd, int mode) {
     assert(fd >= 0);
     io61_file* f = (io61_file*) malloc(sizeof(io61_file));
+    
     f->fd = fd;
+    f->pntr=0;
+    f->cache = (char*) malloc(CACHESIZE*sizeof(char));
+    f->cache_size=0;
+    f->first=-1;
+    f->last=-1;
+    f->size = io61_filesize(f);
+    f->was_seek=0;
+
+
+    file_data = mmap(NULL, f->size, PROT_WRITE, MAP_SHARED, f->fd, 0);
+
+
     (void) mode;
     return f;
 }
@@ -34,11 +68,14 @@ io61_file* io61_fdopen(int fd, int mode) {
 //    Close the io61_file `f` and release all its resources.
 
 int io61_close(io61_file* f) {
-    io61_flush(f);
+    io61_flush(f);    
+    free(f->cache);
+
     int r = close(f->fd);
     free(f);
     return r;
 }
+
 
 
 // io61_readc(f)
@@ -46,12 +83,70 @@ int io61_close(io61_file* f) {
 //    (which is -1) on error or end-of-file.
 
 int io61_readc(io61_file* f) {
+
+    
     unsigned char buf[1];
-    if (read(f->fd, buf, 1) == 1) {
-        return buf[0];
-    } else {
-        return EOF;
+
+    f->flag=0;
+    size_t read_size = 1;
+    off_t safe;
+
+
+    
+
+    //char* file_data = mmap(NULL, 1, PROT_WRITE, MAP_SHARED, f->fd, 0);
+    if (file_data == MAP_FAILED)
+    {
+        //If the whole block is in the cache
+        if(f->first <= (unsigned long long)f->pntr  &&  (unsigned long long)f->pntr <= f->last)
+        {
+            memcpy(buf,  f->cache + (f->pntr - f->first),  1);
+            f->pntr ++;
+
+            f->was_seek=0;
+            //printf("%zu\n", br);
+            //if(br==1)printf("YES\n");
+            return buf[0];
+        }
+
+        if(f->was_seek)
+        {
+            safe = f->pntr;
+            if( (size_t)f->pntr >= CACHESIZE ) io61_seek(f, f->pntr - CACHESIZE+1);
+            else io61_seek(f,0);
+
+        }
+
+        read_size = read(f->fd, f->cache , CACHESIZE);
+
+        f->first = f->pntr;
+        f->last = f->pntr + read_size - 1;
+
+        if(read_size==0)return EOF;
+            
+        if(f->was_seek==0)
+        {
+            memcpy(buf, f->cache, 1); 
+            f->pntr ++;
+            
+        }   
+        else
+        {
+            memcpy(buf, f->cache + CACHESIZE-1, 1);
+            io61_seek(f,safe);
+            f->was_seek=0;
+        }
+
+       return buf[0];
+
+
     }
+
+    memcpy(buf, file_data, 1);
+    f->pntr+=read_size;
+    return buf[0];
+
+
 }
 
 
@@ -62,21 +157,87 @@ int io61_readc(io61_file* f) {
 //    could be read. Returns -1 if an error occurred before any characters
 //    were read.
 
-ssize_t io61_read(io61_file* f, char* buf, size_t sz) {
-    size_t nread = 0;
-    while (nread != sz) {
-        int ch = io61_readc(f);
-        if (ch == EOF) {
-            break;
+ssize_t io61_read(io61_file* f, char* buf, size_t sz)
+{
+    f->flag=0;
+    size_t read_size = min(sz,f->size), nbytes;
+    off_t safe;
+
+
+    //return read(f->fd, buf, sz);
+
+    //file_data = mmap(NULL, sz, PROT_WRITE, MAP_SHARED, f->fd, 0);
+    if (file_data == MAP_FAILED)
+    {
+        f->was_seek=0;
+        //if(sz)printf("%zu\n", sz );
+        //If the whole block is in the cache
+        
+        if(f->first <= (size_t)f->pntr  &&  (size_t)f->pntr + sz - 1  <= f->last)
+        {
+            memcpy(buf,  f->cache + (f->pntr - f->first),  sz);
+            f->pntr += sz;
+            return sz;
         }
-        buf[nread] = ch;
-        ++nread;
+
+        if(f->first > (size_t)f->pntr || f->last < (size_t)f->pntr)
+        {
+            if(f->was_seek)
+            {
+                safe = f->pntr;
+                if( (size_t)f->pntr >= CACHESIZE) io61_seek(f, f->pntr - CACHESIZE + sz);
+                else io61_seek(f,0);
+
+            }
+
+            read_size = read(f->fd, f->cache , CACHESIZE);
+
+            f->first = f->pntr;
+            f->last = f->pntr + read_size - 1;
+            
+            if(f->was_seek==0)
+            {
+                memcpy(buf, f->cache, min(sz,read_size)); 
+                f->pntr += min(sz,read_size);
+            }
+            
+            else
+            {
+                memcpy(buf, f->cache + (safe - f->first), min(sz,read_size));
+                io61_seek(f,safe);
+                f->was_seek=0;
+            }
+
+           
+
+            
+            return min(sz,read_size);
+        }
+
+
+        nbytes = f->last - f->pntr + 1;
+        memcpy(buf, f->cache + (f->pntr - f->first), nbytes);
+
+        read_size = read(f->fd, f->cache , CACHESIZE);
+
+        f->first = f->last+1;
+        f->last = f->last + read_size;
+            
+        memcpy(buf+nbytes, f->cache, min(sz,read_size));
+        
+
+        f->was_seek=0;
+        f->pntr += min(sz,read_size)+nbytes;
+        return min(sz,read_size)+nbytes;
+
     }
-    if (nread != 0 || sz == 0 || io61_eof(f)) {
-        return nread;
-    } else {
-        return -1;
-    }
+
+
+
+    memcpy(buf, file_data, read_size);
+    f->pntr+=read_size;
+    return read_size;
+
 }
 
 
@@ -84,14 +245,11 @@ ssize_t io61_read(io61_file* f, char* buf, size_t sz) {
 //    Write a single character `ch` to `f`. Returns 0 on success or
 //    -1 on error.
 
-int io61_writec(io61_file* f, int ch) {
-    unsigned char buf[1];
-    buf[0] = ch;
-    if (write(f->fd, buf, 1) == 1) {
-        return 0;
-    } else {
-        return -1;
-    }
+int io61_writec(io61_file* f, int ch)
+{
+
+    io61_write(f, (const char*)(&ch), 1);
+    return 0;
 }
 
 
@@ -100,19 +258,30 @@ int io61_writec(io61_file* f, int ch) {
 //    characters written on success; normally this is `sz`. Returns -1 if
 //    an error occurred before any characters were written.
 
-ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
-    size_t nwritten = 0;
-    while (nwritten != sz) {
-        if (io61_writec(f, buf[nwritten]) == -1) {
-            break;
-        }
-        ++nwritten;
+ssize_t io61_write(io61_file* f, const char* buf, size_t sz)
+{
+   
+    f->flag=1;
+    
+    if(f->cache_size+sz>CACHESIZE)
+    {
+        memcpy(f->cache+f->cache_size, buf, CACHESIZE - f->cache_size );
+        write(f->fd, f->cache, CACHESIZE);
+
+
+        memcpy(f->cache, buf + CACHESIZE - f->cache_size, sz - CACHESIZE + f->cache_size );
+
+        f->cache_size = sz - CACHESIZE + f->cache_size;
+
     }
-    if (nwritten != 0 || sz == 0) {
-        return nwritten;
-    } else {
-        return -1;
+    else
+    {
+        memcpy(f->cache+f->cache_size, buf, sz);
+        f->cache_size+=sz;
     }
+
+
+    return sz;
 }
 
 
@@ -121,7 +290,13 @@ ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
 //    If `f` was opened read-only, io61_flush(f) may either drop all
 //    data buffered for reading, or do nothing.
 
-int io61_flush(io61_file* f) {
+int io61_flush(io61_file* f)
+{
+    if(f->flag==1)
+    {
+        write(f->fd, f->cache, f->cache_size);
+        f->cache_size=0;
+    }
     (void) f;
     return 0;
 }
@@ -132,6 +307,11 @@ int io61_flush(io61_file* f) {
 //    Returns 0 on success and -1 on failure.
 
 int io61_seek(io61_file* f, off_t pos) {
+
+    if(f->flag==1){io61_flush(f);}
+    f->was_seek=1;
+
+    f->pntr=pos;
     off_t r = lseek(f->fd, (off_t) pos, SEEK_SET);
     if (r == (off_t) pos) {
         return 0;
